7305cdd23e68af2abc5cf1026f6b01e508ac4288
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-11-01 17:57:15 +0200

    fixed submission, followed instructions

--- python/code_challenges/reverseList/reverseList.py
+++ python/code_challenges/reverseList/reverseList.py
@@ -4,9 +4,10 @@ def reverseList(myList):
    below line is to print the passed list in reversed order
    using built in functions
     """
-    print(myList[::-1])
+    reversedList = myList[::-1]
+    return reversedList
 
 
 if __name__=="__main__":
   myList = list(input())
-  reverseList(myList)
+  print(reverseList(myList))
