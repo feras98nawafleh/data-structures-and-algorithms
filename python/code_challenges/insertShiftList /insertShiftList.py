135e539e48270a4f401aa572131376b36d737dd6
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-11-02 18:35:40 +0200

    finished challenge 02

--- python/code_challenges/insertShiftList /insertShiftList.py
+++ python/code_challenges/insertShiftList /insertShiftList.py
@@ -1,15 +1,26 @@
 import math
 
-def insertShiftlist(myList, number):
+def insertShiftList(myList, number):
     position = math.ceil(len(myList) / 2)
-    myList.insert(position, number)
 
+    newList = [None]*(len(myList) + 1)
+    newList[position] = number
+
+    for index,i in enumerate(myList):
+
+        if newList[index] == None:
+           newList[index]=(i)
+           index += 1
+
+        else:
+            newList[index+1]=(i)
+
+    myList = newList
     return myList
 
 if __name__ == "__main__":
-
-    myList = [1, 2, 3, 4, 6]
-    print(insertShiftlist(myList, 5))
+    myList = [1, 2, 3, 5, 6, 7]
+    print(insertShiftList(myList, 4))
 
 
 
