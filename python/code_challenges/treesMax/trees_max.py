59913c4b3b1b1b4c482e231d0867190b8b7335ea
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-11-22 16:42:39 +0200

    finished challenge 16

--- python/code_challenges/treesMax/trees_max.py
+++ python/code_challenges/treesMax/trees_max.py
@@ -4,7 +4,6 @@ class Node:
         self.left = None
         self.right = None
 
-
 class BinaryTree:
     def __init__(self):
         self.root = None
@@ -23,8 +22,16 @@ class BinaryTree:
         return _traverse
 
     def find_max(self):
-        maxNum = self.pre_order()
-        return (max(maxNum(self.root)))
+        _max = self.root.value
+        def _traverse(root = self.root):
+            nonlocal _max
+            _max = root.value if root.value > _max else _max
+            if root.left:
+                _traverse(root.left)
+            if root.right:
+                _traverse(root.right)
+        _traverse()
+        return _max
 
 def create_tree():
     tree = BinaryTree()
@@ -32,7 +39,7 @@ def create_tree():
     tree.root.left=Node(5)
     tree.root.right=Node(11)
     tree.root.left.left=Node(20)
-    tree.root.left.right=Node(23)
+    tree.root.left.right=Node(50)
     tree.root.right.left=Node(6)
     tree.root.right.right=Node(30)
     return tree
