04af93f2d165e698b8c6a54452b47ae5bc779c50
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-12-12 22:32:46 +0200

    finished challenge 31

---
+++ python/code_challenges/HashMapRepeated/README.md
@@ -0,0 +1,11 @@
+# Challenge Summary
+implement a function that takes a string and returns the very first word to be repeated
+
+## Whiteboard and Process
+[WhiteBoard](https://github.com/feras98nawafleh/data-structures-and-algorithms/blob/main/python/code_challenges/HashMapRepeated/WhiteBoard.png)
+
+## Approach & Efficiency
+BigO = O(n^2)
+
+## Solution
+creating function called repeated takes string as an arguments to return first repeated word.
---
+++ python/code_challenges/HashMapRepeated/hashmaprepeated/linked_list.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+        else:
+            current_value = self.head
+            while current_value.next != None:
+                current_value = current_value.next
+            current_value.next = node
+
+    def __str__(self):
+        output = ""
+        current_value = self.head
+        # print(current_value)
+        while (current_value):
+            output += f"{current_value.value} -> "
+            current_value = current_value.next
+        output += "Null"
+
+        return output
